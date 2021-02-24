c2b7e8d0e49551a7c83e18b073c13e70ccb81280
Антон Николаев <whiteheron819@yandex.ru>
2021-02-24 10:10:48 +0300

    Fixed pep8

--- api/permissions.py
+++ api/permissions.py
@@ -8,8 +8,8 @@ class AdminPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         return bool(
             request.user.is_authenticated and (
-                request.user.is_superuser
-                or request.user.role == User.RoleUser.ADMIN))
+                request.user.is_superuser or
+                request.user.role == User.RoleUser.ADMIN))
 
 
 class GeneralPermission(permissions.BasePermission):
@@ -17,9 +17,9 @@ class GeneralPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         return bool(
             request.user.is_authenticated and (
-                request.user.is_staff
-                or request.user.role == User.RoleUser.ADMIN)
-            or request.method in permissions.SAFE_METHODS)
+                request.user.is_staff or
+                request.user.role == User.RoleUser.ADMIN) or
+            request.method in permissions.SAFE_METHODS)
 
 
 class IsAuthenticated(permissions.BasePermission):
@@ -39,19 +39,5 @@ class IsUser(permissions.BasePermission):
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (request.user.is_superuser == 1
-                or request.user.role == 'admin')
-
-
-class ReviewPermission(permissions.BasePermission):
-
-    def has_object_permission(self, request, view, obj):
-        return bool(request.method in permissions.SAFE_METHODS or
-                    obj.author == request.user)
-
-
-class ModeratorPermission(permissions.BasePermission):
-
-    def has_object_permission(self, request, view, obj):
-        return bool(request.method in permissions.SAFE_METHODS or
-                    request.user.role == User.RoleUser.MODERATOR)
+        return (request.user.is_superuser == 1 or
+                request.user.role == 'admin')
